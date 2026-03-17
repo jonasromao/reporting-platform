2d05acfd931b14788a0bae81cae8dada42b2efc1
Rodrigo Cabral <rodrigoalcab@gmail.com>
2026-03-17 17:30:41 +0000

    Criado componente alerta

--- resources/views/schools/index.blade.php
+++ resources/views/schools/index.blade.php
@@ -1,11 +1,7 @@
 <div>
     <h2>Listar as Escolas</h2>
 
-    @if(session('success'))
-        <p style="color: #082">
-            {{ session('success') }}
-        </p>
-    @endif
+    <x-alert/>
 
     <a href="{{ route('schools.create') }}">Cadastrar</a>
 </div>
