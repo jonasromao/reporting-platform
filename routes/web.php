e10e84ad359c6a26ddf0783906d33e4216ea6ba4
Rodrigo Cabral <rodrigoalcab@gmail.com>
2026-03-23 17:04:33 +0000

    Home controller criado
    Foi elimitado o arquivo welcome que existia no início, agora sendo adaptada a página inicial sendo acessada por controller, não mais por rota direta

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use App\Http\Controllers\ClassroomController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SchoolController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 // Tela de login
 
